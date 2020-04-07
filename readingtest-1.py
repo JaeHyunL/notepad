777e1228f265ad5d2a9965fb09658dad17cb7235
LEEjaeHyun <swaa23@gmail.com>
2020-04-07 15:11:38 +0900

    마우스 이동방향 표기 키보드 입력 추가

--- readingtest-1.py
+++ readingtest-1.py
@@ -13,12 +13,27 @@ movey = 0
 time1 = 0
 time2 = 0
 clickenvet = 0
-Coordinates = 0
+
+
+def checkcoordinates(a, b):
+    coordinates = 0
+    if movex <= 0 and movey >= 0:
+        coordinates = 1  # 오른쪽 상단으로 이동  1사분면
+    elif movex >= 0 and movey >= 0:
+        coordinates = 2  # 왼쪽 상단으로 이동  2사분면
+    elif movex >= 0 and movey <= 0:
+        coordinates = 3  # 왼쪽 하단으로 이동  3사분면
+    elif movex <= 0 and movey <= 0:
+        coordinates = 4  # 오른쪽 하단으로 이동  4사분면
+    return coordinates
+
+
 # 이동
 for line in rdr:
     # try:
     #    if line[8] == 'Button.left':
-    #        clickenvet = 1
+    #        clicke
+    # nvet = 1
     #    elif line[8] == 'Button.right':
     #        clickenvet = 2
     # except IndexError:
@@ -26,25 +41,19 @@ for line in rdr:
     x = int(line[6])
     y = int(line[7])
     time1 = float(line[0])
-    movex = (x2 - x)
+    # 좌표 이동
+    movex = (x - x2)
 # 미세한 좌표 이동값은 무시
     if abs(movex) > 3 or abs(movey) > 3:
-        if movex > 0 and movey > 0:
-            Coordinates = 1
-        elif movex < 0 and movey > 0:
-            Coordinates = 2
-        elif movex < 0 and movey < 0:
-            Coordinates = 3
-        elif movex > 0 and movey < 0:
-            Coordinates = 4
+
         # Coordinates 해당 좌표 평면으로 아래 인자값만큼 이동
         wr.writerow([abs(movex)+abs(movey),
-                     abs(time2-time1)*1000000, Coordinates])
+                     abs(time2-time1)*1000, checkcoordinates(movex, movey)])
 
-    movey = (y2 - y)
+    movey = (y - y2)
     time2 = time1
     y2 = y
     x2 = x
-    #clickenvet = None
+    # clickenvet = None
 f.close()
 f2.close()
--- testing1-1.py
+++ testing1-1.py
@@ -1,56 +1,82 @@
-from pynput.mouse import Listener
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Key, Listener as KeyboardListener
 from datetime import datetime, timezone, timedelta
 import time
 import csv
 
 
 f = open('mouse_logs.csv', 'w', newline='')
-
+f2 = open('keyboard_logs.csv', 'w', newline='')
 # TODO datetime.now.strtime 함수화 사용
 wr = csv.writer(f)
 
 KST = timezone(timedelta(hours=9))
-
+wr2 = csv.writer(f2)
 now = datetime.now(KST)
 
 
-def timeup(self, year, hour, mit, sec, mil):
-    self.year = datetime.now(KST).strftime("%Y%m%d")
-    self.hour = datetime.now(KST).strftime("%H")
-    self.mit = datetime.now(KST).strftime("%Y%m%d")
-    self.sec = datetime.now(KST).strftime("%S")
-    self.sec = datetime.now(KST).strftime("%f")
+class update():
+    def year(self):
+        return datetime.now(KST).strftime("%Y%m%d")
+
+    def hour(self):
+        return datetime.now(KST).strftime("%H")
+
+    def mit(self):
+        return datetime.now(KST).strftime("%M")
+
+    def sec(self):
+        return datetime.now(KST).strftime("%S")
+
+    def mil(self):
+        return datetime.now(KST).strftime("%f")
 
 
 def on_move(x, y):
     print(x, y)
 
-    wr.writerow([timeup(),  x, y])
+    wr.writerow([time.time(), update().year(), update().hour(),
+                 update().mit(), update().sec(), update().mil(),  x, y])
     print(datetime.now(KST).strftime('%Y-%m-%d %H:%M:%S.%f'))
 
 
 def on_click(x, y, button, pressed):
-    pass
-    mil = datetime.now(KST).strftime("%f")
 
-    wr.writerow([timeup(year), x, y, button])
+    wr.writerow([time.time(), update().year(), update().hour(),
+                 update().mit(), update().sec(), update().mil(),  x, y, button])
 
     # logging.info('Mouse clicked at ({0} , {1}) with{2}'.format(x, y, button))
 
 
 def on_scroll(x, y, dx, dy):
     # logging.info('Mouse scrolled at ({0},{1}) ({2},{3})'.format(x, y, dx, dy))
-    pass
-    wr.writerow([year, hour, mit, sec, mil,  x, y, dx, dy])
 
-    year = datetime.now(KST).strftime("%Y%m%d")
-    hour = datetime.now(KST).strftime("%H")
-    mit = datetime.now(KST).strftime("%M")
-    sec = datetime.now(KST).strftime("%S")
-    mil = datetime.now(KST).strftime("%f")
+    wr.writerow([time.time(), update().year(), update().hour(),
+                 update().mit(), update().sec(), update().mil(),  x, y, dx, dy])
 
 
-with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+def on_press(key):
+
+    wr2.writerow([time.time(), update().year(), update().hour(),
+                  update().mit(), update().sec(), update().mil(), key])
+
+    print(key)
+
+
+def on_release(key):
+
+    wr2.writerow([time.time(), update().year(), update().hour(),
+                  update().mit(), update().sec(), update().mil(), key])
+
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+
+with MouseListener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+    listener.join()
+with KeyboardListener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
 
-    f.close()
+f.close()
+f2.close()
